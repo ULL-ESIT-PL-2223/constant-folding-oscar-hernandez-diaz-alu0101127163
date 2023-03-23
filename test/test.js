2811186064ec5fabd11cb74decf4f68c9c5d015f
alu0101127163 <alu0101127163@ull.edu.es>
2023-03-23 20:04:41 +0000

    TEst

--- test/test.js
+++ test/test.js
@@ -1,16 +1,6 @@
 const constantFolding = require('../src/constant-folding');
 const should = require('chai').should();
 
-const example1 = `a=2*3;`;
-const expectedOutput = `a = 6;`;
-
-describe('constantFolding tests', () => {
-  it('works correctly with normal functions', () => {
-    constantFolding(example1).should .equal(expectedOutput);
-  });
-
-});
-
 const Checks = [
   { text: "a=2*3;", result: "a = 6;"},
   { text: "[a, b, c].join();", result: "'a,b,c';" },
